45fc3fc62f2a75868c97f30b19dfab0b49a653a6
nikitabrazhnikov@mail.ru <nikitabrazhnikov@mail.ru>
2025-03-27 22:17:01 +0700

    Создал класс игры, позволяющий выполнить тест

--- TestirovanieLR3/test.cpp
+++ TestirovanieLR3/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 
+class Game {
+public:
+    Game() {};
+};
 
-//тест на создание класса игры
+
+//test na sozdanie classa igry
 TEST(gameClassCreationTest, CreationClass) {
     Game* myGame = new Game();
     ASSERT_NE(myGame, nullptr);
