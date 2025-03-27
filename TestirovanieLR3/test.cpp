5c62c1b7bbf2178c987d6b7dfd482a6838d274a4
nikitabrazhnikov@mail.ru <nikitabrazhnikov@mail.ru>
2025-03-27 23:01:31 +0700

    Добавил еще 2 вопроса, доработал тест. Теперь он принимает аргумент - номер вопроса.

--- TestirovanieLR3/test.cpp
+++ TestirovanieLR3/test.cpp
@@ -20,6 +20,8 @@ string Game::start() //formirovanie soobsheniya o nachale igry
 
 string Game::askAQuestion() //formirovanie soobsheniya o nachale igry
 {
+    //todo добавить больше вопросов
+    //todo хранение вопросов в файле
     return "Сколько будет 2+2?";
 }
 
@@ -37,10 +39,23 @@ TEST(gameClassStartTest, StartGame) {
     ASSERT_EQ(startMessage, "Добро пожаловать на игру Кто хочет стать миллионером! Нажмите enter, чтобы начать.");
 }
 
-//test zadit vopros
-TEST(gameClassAskingTest, AskAQuestion) {
-    //todo сделать больше одного вопроса
+//test zadit vopros nomer 1
+TEST(gameClassAskingTest, AskAQuestion1) {
     Game* myGame = new Game();
-    string questionText = myGame->askAQuestion();
+    string questionText = myGame->askAQuestion(1);
     ASSERT_EQ(questionText, "Сколько будет 2+2?");
+}
+
+//test zadit vopros nomer 2
+TEST(gameClassAskingTest, AskAQuestion2) {
+    Game* myGame = new Game();
+    string questionText = myGame->askAQuestion(2);
+    ASSERT_EQ(questionText, "В каком месяце 28 дней?");
+}
+
+//test zadit vopros nomer 3
+TEST(gameClassAskingTest, AskAQuestion) {
+    Game* myGame = new Game();
+    string questionText = myGame->askAQuestion(3);
+    ASSERT_EQ(questionText, "Номер счета Материалы в плане счетов бухгалтерского учета ...");
 }
